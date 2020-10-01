cf738621c6a565574de411c866c7266e47ac7870
manitmaini <58745300+manitmaini@users.noreply.github.com>
2020-10-01 09:41:35 +0530

    GETTING used with git.....
    Presented properly....

--- spamming.py
+++ spamming.py
@@ -1,6 +1,8 @@
+# MAKE SURE WHEN U RUN THE CODE U MUST HAVE CLICKED THE MESSAGE AREA IN WHATSAPP, HAPPY SPAMMING!!
+# BEGINNER PROJECT...
 import pyautogui
 import time
 while True:
-    pyautogui.typewrite("Hello mummy ji kese ho")
+    pyautogui.typewrite("SPAM TEXT TO BE WRITTEN HERE")
     time.sleep(3)
     pyautogui.press("enter")
